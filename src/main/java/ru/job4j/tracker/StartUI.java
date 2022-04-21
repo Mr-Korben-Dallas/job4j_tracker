21d2c7d9a618d74959f93738200d8a92f9c0e324
Zolotukhin Vladimir <daco.inc.v@gmail.com>
2022-04-21 14:42:05 +0400

    [#500745]: - добавлена логика первого пункта меню в метод init() в StartUI.java;

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -16,6 +16,16 @@ public class StartUI {
                 Item item = new Item(name);
                 tracker.add(item);
                 System.out.println("You added item: " + item);
+            } else if (select == 1) {
+                System.out.println("=== Show all items ===");
+                Item[] items = tracker.findAll();
+                if (items.length > 0) {
+                    for (Item item : items) {
+                        System.out.println(item);
+                    }
+                } else {
+                    System.out.println("The Tracker does not yet contain any items");
+                }
             } else if (select == 6) {
                 run = false;
             }
