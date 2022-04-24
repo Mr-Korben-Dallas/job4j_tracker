a79ab8db63ca20ac23e2b3d79678be4dae7d1094
Zolotukhin Vladimir <daco.inc.v@gmail.com>
2022-04-24 10:29:39 +0400

    [#41598]: - имплементирована логика поиска в ArrayList; - добавлены тесты;

---
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.search;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class PhoneDictionaryTest {
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(new Person("Petr", "Arsentev", "534872", "Bryansk"));
+        phones.add(new Person("John", "Doe", "12345", "Moskow"));
+        ArrayList<Person> persons = phones.find("Petr");
+        assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+
+    @Test
+    public void whenNoOneFound() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(new Person("Petr", "Arsentev", "534872", "Bryansk"));
+        phones.add(new Person("John", "Doe", "12345", "Moskow"));
+        ArrayList<Person> persons = phones.find("Patrick");
+        assertTrue(persons.isEmpty());
+    }
+}
