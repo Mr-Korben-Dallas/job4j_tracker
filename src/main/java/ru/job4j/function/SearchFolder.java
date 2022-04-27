bc562da1dc2c385b14013da2a6323a08227c0b49
Zolotukhin Vladimir <daco.inc.v@gmail.com>
2022-04-27 10:24:23 +0400

    [#249209]: - имплементирована логика поиска папок; - добавлены тесты;

---
+++ src/main/java/ru/job4j/function/SearchFolder.java
@@ -0,0 +1,18 @@
+package ru.job4j.function;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class SearchFolder {
+    public static List<Folder> filter(List<Folder> list, Predicate<Folder> pred) {
+        List<Folder> result = new ArrayList<>();
+        for (Folder listItem : list) {
+            if (pred.test(listItem)) {
+                result.add(listItem);
+            }
+        }
+        return result;
+    }
+}
