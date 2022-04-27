8caa4c54265abf11c4c418215b62e2ae7e7c72f6
Zolotukhin Vladimir <daco.inc.v@gmail.com>
2022-04-27 12:16:12 +0400

    [#24255]: - рефакторинг метода find();

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,6 +1,7 @@
 package ru.job4j.search;
 
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
 public class PhoneDictionary {
     private ArrayList<Person> persons = new ArrayList<>();
@@ -15,13 +16,14 @@ public class PhoneDictionary {
      * @return Список подошедщих пользователей.
      */
     public ArrayList<Person> find(String key) {
+        Predicate<Person> predicateName = el -> el.getName().contains(key);
+        Predicate<Person> predicatePhone = el -> el.getPhone().contains(key);
+        Predicate<Person> predicateSurname = el -> el.getSurname().contains(key);
+        Predicate<Person> predicateAdress = el -> el.getAddress().contains(key);
+        Predicate<Person> combine = predicateName.or(predicatePhone).or(predicateAdress).or(predicateSurname);
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
-            if (person.getName().contains(key)
-                    || person.getSurname().contains(key)
-                    || person.getAddress().contains(key)
-                    || person.getPhone().contains(key)
-            ) {
+            if (combine.test(person)) {
                 result.add(person);
             }
         }
